f41d0b42c7c17d660132da58d552317782f22efb
Francis Fernandez <francisfernandez0679@gmail.com>
2023-11-27 20:23:36 +0800

    Added insertion sort

--- src/algorithms/insertion_sort.js
+++ src/algorithms/insertion_sort.js
@@ -1,5 +1,20 @@
-import { sortDelay } from '@/utils'
+import { sortDelay, setIgnoredItemsState } from '@/utils'
 
 export default async function insertionSort(n, arr, getDelay) {
-  
+  let j, item
+  for (let i = 1; i < n; i++) {
+    item = arr[i].value
+    arr[i].current = true
+    for (j = i - 1; j >= 0 && arr[j].value > item; j--) {
+      arr[j].picked = true
+      arr[j + 1].value = arr[j].value
+      await sortDelay(getDelay())
+      arr[j].picked = false
+    }
+    await sortDelay(getDelay())
+    arr[j + 1].value = item
+    arr[i].current = false
+  }
+
+  setIgnoredItemsState(arr)
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
 export const sortDelay = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-export const setIgnoredItemsState = (arr) => {
+export const setIgnoredItemsState = async (arr) => {
   const items = arr.filter((data) => !data.sorted)
-  items.forEach((item) => (item.sorted = true))
+  for (let i = 0; i < items.length; i++) {
+    items[i].sorted = true
+    await sortDelay(10)
+  }
 }
